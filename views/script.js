bd97da9c39088f1b01060374265990c6a0e55574
Luke Livingstone <lukel9914@gmail.com>
2017-11-23 16:21:39 +0000

    fixes

--- views/script.js
+++ views/script.js
@@ -8,24 +8,25 @@ const songs = [];
 const songList = document.getElementById('songList');
 
 let currentlyPlaying;
+let player;
 let volume;
 
 function readURL () {
   setTimeout(() => {
     const url = document.getElementById('urlBox').value;
     if (ytrx.test(url)) {
-      downloadFile(url);
+      getTrackInfo(url);
+      document.getElementById('urlBox').value = '';
     }
   }, 100);
 }
 
-async function downloadFile (url) {
+async function getTrackInfo (url) {
   const info = await ytdl.getInfo(url);
 
   const song = {
     title: info.title,
-    url: getBestStream(filterOpus(info.formats)).url,
-    index: songs.length
+    url: getBestStream(filterOpus(info.formats)).url
   }
 
   songs.push(song);
@@ -33,21 +34,27 @@ async function downloadFile (url) {
 }
 
 function playSong (song) {
-  if (currentlyPlaying) {
-    currentlyPlaying.pause();
-  }
-  currentlyPlaying = new Audio(song.url);
-  currentlyPlaying.volume = volume || 1;
-  currentlyPlaying.onended = () => {
-    const nextTrack = (song.index >= songs.length - 1 ? 0 : songs[song.index + 1]);
-    playSong(nextTrack);
-  };
-  currentlyPlaying.play();
+  if (player) 
+    player.pause();
 
-  let parent = document.querySelector(`.song[index="${song.index}"]`);
-  if (parent.className.includes('fadein')) {
+  currentlyPlaying = song;
+  player = new Audio(song.url);
+  player.volume = volume || 1;
+  player.onended = () => {
+    const currentIndex = songs.indexOf(currentlyPlaying);
+    if (currentIndex === -1 && songs.length > 0 || currentIndex === songs.length - 1)
+      return playSong(songs[0]);
+
+    return playSong(songs[currentIndex++]);
+  };
+
+  const trackIndex = songs.indexOf(currentlyPlaying);
+  player.play();
+
+  let parent = document.querySelector(`.song[index="${trackIndex}"]`);
+  if (parent.className.includes('fadein'))
     parent.className = parent.className.replace('fadein ', '');
-  }
+
   const playing = document.querySelector('.playing');
   if (playing) {
     playing.className = playing.className.replace('playing', 'playing-end');
@@ -55,12 +62,13 @@ function playSong (song) {
       playing.className = playing.className.replace('playing-end', '');
     }, 750);
   }
-  document.querySelector(`.song[index="${song.index}"]`).className += ' playing';
+  document.querySelector(`.song[index="${trackIndex}"]`).className += ' playing';
 }
 
 function renderSongDiv (song) {
+  const trackIndex = songs.indexOf(song);
   const parent = document.createElement('div');
-  parent.setAttribute('index', song.index);
+  parent.setAttribute('index', trackIndex);
   parent.className = 'song fadein container level';
   const children = [];
 
@@ -109,30 +117,36 @@ function PlayPause () {
     return playSong(songs[0]);
   }
 
-  const playingSong = document.querySelector(`div[index="${songs.indexOf(currentlyPlaying.src.slice(7).replace(/%20/g, ' '))}"]`);
+  const playingSong = document.querySelector(`div[index="${songs.indexOf(currentlyPlaying)}"]`);
 
-  if (currentlyPlaying.paused) {
+  if (player.paused) {
     button.innerHTML = 'Pause';
     playingSong.className = playingSong.className.replace('paused', 'playing')
-    currentlyPlaying.play();
+    player.play();
   } else {
     playingSong.className = playingSong.className.replace('playing', 'playing-end');
     setTimeout(() => {
       playingSong.className = playingSong.className.replace('playing-end', 'paused');
     }, 750);
     button.innerHTML = 'Play';
-    currentlyPlaying.pause();
+    player.pause();
   }
 }
 
 function playNext () {
-  const currentIndex = songs.indexOf(currentlyPlaying.src.slice(7).replace(/%20/g, ' '));
-  playSong(currentIndex >= songs.length - 1 ? 0 : currentIndex + 1);
+  const currentIndex = songs.indexOf(currentlyPlaying);
+  if (currentIndex === -1 && songs.length > 0 || currentIndex === songs.length - 1)
+    return playSong(songs[0]);
+
+  return playSong(songs[currentIndex++]);
 }
 
 function playPrev () {
-  const currentIndex = songs.indexOf(currentlyPlaying.src.slice(7).replace(/%20/g, ' '));
-  playSong(currentIndex - 1 < 0 ? songs.length - 1 : currentIndex - 1);
+  const currentIndex = songs.indexOf(currentlyPlaying);
+  if (currentIndex === -1 && songs.length > 0 || currentIndex === 0)
+    return playSong(songs[songs.length - 1]);
+
+  return playSong(songs[currentIndex--]);
 }
 
 function filterOpus(formats) {
